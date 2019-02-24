ede6d6acfc6504d67d7c0a688ba441f703008b3a
Raphael Ferras <raphaelferras@gmail.com>
2019-02-24 11:50:11 -0500

    Adding styles to the chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,12 +28,12 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
-
+  var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage',{
     from:'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function(resMessage) {
-    console.log(resMessage);
+    messageTextbox.val('')
   });
 });
 
@@ -43,12 +43,19 @@ locationButton.on('click', function() {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browers');
   }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Sending location');;
+
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
   }, function () {
-    return alert('Unable to fetch location');
+    locationButton.removeAttr('disabled');
+
+    return alert('Unable to fetch location').text('Sending location');;
   })
 });
